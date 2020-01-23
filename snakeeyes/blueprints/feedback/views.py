da608cc64a8cb2eb593e770c0aea655d1fdf6f2b
Przemek Gawlowski <przemek.gawlowski@gamelounge.com>
2020-01-23 13:52:57 +0100

    Send a feedback email with POST request

--- snakeeyes/blueprints/feedback/views.py
+++ snakeeyes/blueprints/feedback/views.py
@@ -15,4 +15,13 @@ feedback = Blueprint('feedback', __name__, template_folder='templates')
 def index():
     form = FeedbackForm()
 
+    if form.validate_on_submit():
+        # This prevents circular imports.
+        from snakeeyes.blueprints.feedback.tasks import deliver_feedback_email
+
+        deliver_feedback_email.delay(request.form.get('email'), request.form.get('message'))
+
+        flash('Thanks for your feedback!', 'success')
+        return redirect(url_for('feedback.index'))
+
     return render_template('feedback/index.html', form=form)
